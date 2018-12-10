153abe9c7e7c6cfd260b363114acf4cc2f4da54a
Marcus Bittencourt <ma_rcus@hotmail.com>
2018-12-10 00:57:17 -0200

    correção do arquivo de rotas para nova hierarquia do projeto

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,12 +1,12 @@
-import { SignUpComponent } from './sign-up/sign-up.component';
+import { SignUpComponent } from './auth/sign-up/sign-up.component';
 import { RouterModule, Routes } from '@angular/router';
 import { ModuleWithProviders } from '@angular/core/src/metadata/ng_module';
 
 
 export const routes: Routes = [
-    { 
-        path: 'sign-up', 
-        component: SignUpComponent 
+    {
+        path: 'sign-up',
+        component: SignUpComponent
     },
 ]
 export const ROUTING: ModuleWithProviders = RouterModule.forRoot(routes);
