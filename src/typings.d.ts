c5142ee850a991e484aa87d11ac0996f87da3ff0
Marcus Bittencourt <ma_rcus@hotmail.com>
2019-03-03 20:08:08 -0300

    importação de arquivos json pelo typescript habilitada

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,2 +1,12 @@
 // so the typescript compiler doesn't complain about the global config object
 declare var config: any;
+
+declare module '*.json' {
+  const value: any;
+  export default value;
+}
+
+declare module 'json!*' {
+  const value: any;
+  export default value;
+}
