f676ef594b6bf78ed28adeab1e766d3ac15c5ae6
Marcus Bittencourt <ma_rcus@hotmail.com>
2019-03-03 21:35:30 -0300

    isolamento da api node para persistência no mongo

--- server.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-const app = require('./src/persistence/organizations.mongoose');
-
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-    console.info(`Api inicializada com sucesso na porta ${port}`);
-});
