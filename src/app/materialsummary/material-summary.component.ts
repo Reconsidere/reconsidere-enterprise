d2ea4d06e23c58e4f002bd5a5d454f6628759981
vinicius-santos <vinicius_bauer@hotmail.com>
2019-04-08 16:25:56 -0300

    finalizando resumo de materiasi para teste
    finalizando resumo de materiasi para teste

--- src/app/materialsummary/material-summary.component.ts
+++ src/app/materialsummary/material-summary.component.ts
@@ -46,7 +46,7 @@ export class MaterialSummaryComponent implements OnInit {
     if (id !== undefined) {
       this.entriesService.getEntries(this.organizationId).subscribe(items => this.loadAll(items), error => error);
     } else {
-      // this.toastr.warning(messageCode['WARNNING']['WRE007']['summary']);
+      this.toastr.warning(messageCode['WARNNING']['WRE016']['summary']);
       this.isBlocked = false;
       return;
     }
@@ -54,7 +54,7 @@ export class MaterialSummaryComponent implements OnInit {
 
   loadAll(items) {
     if (items === undefined || items.length <= 0) {
-      // this.toastr.warning(messageCode['WARNNING']['WRE007']['summary']);
+      this.toastr.warning(messageCode['WARNNING']['WRE016']['summary']);
       this.isBlocked = false;
       return;
     } else {
@@ -65,34 +65,7 @@ export class MaterialSummaryComponent implements OnInit {
     }
   }
 
-  groupBy(key) {
-    var names = [];
-    this.entriesResult = this.entries.reduce(function (rv, x) {
-      (rv[x[key]] = rv[x[key]] || []).push(x);
-      if (names.indexOf(x[key]) === -1) {
-        names.push(x[key]);
-      }
-      return rv;
-    }, {});
-    return this.addToDictionary(names);
-    return this.entriesResult;
 
-
-  }
-
-  addToDictionary(names) {
-    var dictionary = [];
-    names.forEach(name => {
-      dictionary.push({
-        key: name,
-        value: this.entriesResult[name]
-      });
-    });
-
-    this.calculateValues(Entries.types.purchase, names);
-    this.calculateValues(Entries.types.sale, names);
-    return dictionary;
-  }
 
   createSimpleList(list: Entries) {
     this.insertItems(Entries.types.purchase, Entries.Type.Input, list);
@@ -125,35 +98,106 @@ export class MaterialSummaryComponent implements OnInit {
   }
 
 
-  calculateValues(type, names) {
-    this.calculateTotal(this.entriesResult, names);
-    this.calculateAverage(this.entriesResult, names);
-    //this.calculateWeight(this.entriesResult);
+  groupBy(key) {
+    var names = [];
+    var results = [];
+    this.entriesResult = this.reduce(names, key, this.entries);
+    this.sum(Entries.types.sale, names, results);
+    this.sum(Entries.types.purchase, names, results);
+    this.entriesResult = results;
+    this.entriesResult = this.reduce(names, key, this.entriesResult);
     this.calculateQuantityStock(this.entriesResult, names);
+    this.calculateWeightStock(this.entriesResult, names);
+    this.calculateTotal(this.entriesResult, names);
+    this.calculateAverage(this.entriesResult, names, Entries.types.purchase);
+    this.calculateAverage(this.entriesResult, names, Entries.types.sale);
+
+    this.entriesResult = this.addToDictionary(names);
+    return this.entriesResult;
   }
+
+  reduce(names, key, items) {
+    return items.reduce(function (rv, x) {
+      (rv[x[key]] = rv[x[key]] || []).push(x);
+      if (names.indexOf(x[key]) === -1) {
+        names.push(x[key]);
+      }
+      return rv;
+    }, {});
+  }
+
+  sum(type, names, results) {
+    names.forEach(name => {
+      let object = { _id: undefined, amount: 0.0, type: type, name: undefined, quantity: 0.0, average: 0.0, weight: 0.0, expand: undefined, typeEntrie: undefined, stock: 0.0 };
+
+      this.entriesResult[name].forEach(item => {
+        if (item.type === type) {
+          object.amount += item.amount;
+          object.quantity += item.quantity;
+          object.weight += item.weight;
+          object._id = item._id;
+          object.type = item.type;
+          object.name = item.name;
+          object.typeEntrie = item.typeEntrie;
+          object.expand = true;
+
+        } else {
+          return;
+        }
+      });
+      if (object._id !== undefined) {
+        results.push(object);
+      }
+    });
+    return results;
+  }
+
+
+
+  addToDictionary(names) {
+    var dictionary = [];
+    names.forEach(name => {
+      dictionary.push({
+        key: name,
+        value: this.entriesResult[name]
+      });
+    });
+    return dictionary;
+  }
+
+
 
 
   calculateTotal(items, names) {
+    let totPurchase;
+    let totSale;
     names.forEach(name => {
-      items[name].forEach(item => {
-        if (item.total === undefined || item.total === NaN) {
-          item.total = 0.0;
+      let filterPurchase = items[name].filter(x => x.type === Entries.types.purchase && x.name === name);
+      let filterSale = items[name].filter(x => x.type === Entries.types.sale && x.name === name);
+      totPurchase = this.sumValues('amount', filterPurchase);
+      totSale = this.sumValues('amount', filterSale);
+      items[name].forEach(res => {
+        res.total = totPurchase - totSale;
+        res.total = Math.abs(res.total);
+      });
+    });
+
+  }
+
+  calculateAverage(items, names, type) {
+    names.forEach(name => {
+      let quantity = this.sumValues('quantity', items[name].filter(x => x.name === name && x.type === type));
+      if (quantity === 0) {
+        return;
+      }
+      items[name].forEach(res => {
+        if (res.type === type) {
+          let average = res.amount / quantity;
+          res.average = Math.abs(average);
         }
-        item.total += item.amount;
       });
     });
   }
-
-  calculateAverage(items, names) {
-    names.forEach(name => {
-      items[name].forEach(item => {
-        let quantity = this.sumQuantity('quantity', this.entries.filter(x => x.name === item.name));
-        item.average = item.total / quantity;
-
-      });
-    });
-  }
-
 
   calculateQuantityStock(items, names) {
     let totPurchase;
@@ -161,8 +205,8 @@ export class MaterialSummaryComponent implements OnInit {
     names.forEach(name => {
       let filterPurchase = items[name].filter(x => x.type === Entries.types.purchase && x.name === name);
       let filterSale = items[name].filter(x => x.type === Entries.types.sale && x.name === name);
-      totPurchase = this.sumQuantity('quantity', filterPurchase);
-      totSale = this.sumQuantity('quantity', filterSale);
+      totPurchase = this.sumValues('quantity', filterPurchase);
+      totSale = this.sumValues('quantity', filterSale);
       items[name].forEach(res => {
         res.stock = totPurchase - totSale;
         res.stock = Math.abs(res.stock);
@@ -171,48 +215,23 @@ export class MaterialSummaryComponent implements OnInit {
 
   }
 
+  calculateWeightStock(items, names) {
+    let totPurchase;
+    let totSale;
+    names.forEach(name => {
+      let filterPurchase = items[name].filter(x => x.type === Entries.types.purchase && x.name === name);
+      let filterSale = items[name].filter(x => x.type === Entries.types.sale && x.name === name);
+      totPurchase = this.sumValues('weight', filterPurchase);
+      totSale = this.sumValues('weight', filterSale);
+      items[name].forEach(res => {
+        res.weightStock = totPurchase - totSale;
+        res.weightStock = Math.abs(res.weightStock);
+      });
+    });
 
+  }
 
-
-  // calculateWeight(items, names) {
-
-  // }
-
-
-  // generteValuesPurchase(items, type) {
-  //   if (items === undefined || items.length <= 0) {
-  //     return;
-  //   }
-  //   let result = [];
-  //   items.reduce(function (res, value) {
-  //     if (!res[value.name]) {
-  //       res[value.name] = { _id: value._id, total: value.amount, type: value.type, name: value.name, quantity: value.quantity, average: 0.0, weight: value.weight, expand: true, typeEntrie: value.typeEntrie, stock: 0.0 };
-  //       result.push(res[value.name]);
-  //     }
-
-  //     res[value.name].total += value.amount;
-  //     res[value.name].weight += value.weight;
-  //     return res;
-  //   }, {});
-  //   this.calculateAverage(result);
-  //   this.calculateQuantityStock(result, this.entries.filter(x => x.type === type));
-
-  //   if (this.entriesResult === undefined || this.entriesResult.length <= 0) {
-  //     this.entriesResult = result;
-  //   } else {
-  //     this.addMore(result);
-  //   }
-  // }
-
-  // addMore(result) {
-  //   result.forEach(item => {
-  //     this.entriesResult.push(item);
-  //   });
-  // }
-
-
-
-  sumQuantity(prop, items) {
+  sumValues(prop, items) {
     try {
       return items.reduce(function (a, b) {
         return a + b[prop];
@@ -221,27 +240,6 @@ export class MaterialSummaryComponent implements OnInit {
       return 0;
     }
   }
-
-  // separeteGroups() {
-  //   this.entriesResult.forEach(result => {
-  //     if (result.type === Entries.types.purchase) {
-  //       if (this.entriesPurchase === undefined || this.entriesPurchase.length <= 0) {
-  //         this.entriesPurchase = [result];
-  //       } else {
-  //         this.entriesPurchase.push(result);
-  //       }
-  //     }
-  //     else if (result.type === Entries.types.sale) {
-  //       if (this.entriesSale === undefined || this.entriesSale.length <= 0) {
-  //         this.entriesSale = [result];
-  //       } else {
-  //         this.entriesSale.push(result);
-  //       }
-  //     }
-  //   });
-  // }
-
-
 
   closeOrExpand(item) {
     if (item.expand) {
